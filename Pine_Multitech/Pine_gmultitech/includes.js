1c3f4f18ec8a3cf4c11f7d0ce10c69311b26cdef
missmetal <mmartinez@est.frba.utn.edu.ar>
2016-08-28 22:07:46 -0300

    Armado de prototipo de pagina de producto
    falta ajustar scripts

--- Pine_Multitech/Pine_gmultitech/includes.js
+++ Pine_Multitech/Pine_gmultitech/includes.js
@@ -9,9 +9,15 @@ function includeJs(path){
 includeJs("js/jquery.js");
 includeJs("js/bootstrap.js");
 includeJs("js/bootstrap.min.js");
+includeJs("js/jquery.prettyPhoto.init.min.js");
+includeJs("js/jquery.prettyPhoto.init.js");
+includeJs("js/jquery.prettyPhoto.js");
+includeJs("js/jquery.prettyPhoto.min.js");
 
 /*Hojas de estilo*/
 includeCss("css/styleHomepage.css");
 includeCss("css/bootstrap.css");
 includeCss("css/bootstrap.min.css");
 includeCss("css/font-awesome.min.css");
+includeCss("css/woocommerce-custom.css");
+includeCss("css/prettyPhoto.css");
